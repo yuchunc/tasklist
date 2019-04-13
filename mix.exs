07b492fe75639ca85a21287761d28f543dd330d8
Mickey <poverwhelming@gmail.com>
2019-04-13 15:51:40 +0800

    added absinthe

--- mix.exs
+++ mix.exs
@@ -42,7 +42,10 @@ defmodule Tasklist.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.4.0"},
+      {:absinthe_plug, "~> 1.4.0"},
+      {:poison, "~> 3.1"} #This is a hack
     ]
   end
 
